0f1430a5c5762bcbb536d44d53b1a9d51cd90a43
Valto Silva <valtosilva7@gmail.com>
2023-03-02 12:04:55 -0300

    Fixed Image src

--- client/src/components/form/Image.jsx
+++ client/src/components/form/Image.jsx
@@ -2,7 +2,7 @@ export default function Image({ src, ...rest }) {
   src =
     src && src.includes("https://")
       ? src
-      : "http://localhost:4000/uploads/" + src;
+      : "https://vs-airbnb.vercel.app/uploads/" + src;
 
   return <img {...rest} src={src} />;
 }
