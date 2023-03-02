e3d08ac56b2d26681c3b50e5c57e2a2598d74877
Valto Silva <valtosilva7@gmail.com>
2023-03-02 12:10:07 -0300

    Vercel media image

--- client/src/components/form/Image.jsx
+++ client/src/components/form/Image.jsx
@@ -2,7 +2,7 @@ export default function Image({ src, ...rest }) {
   src =
     src && src.includes("https://")
       ? src
-      : "https://vs-airbnb.vercel.app/uploads/" + src;
+      : "https://vs-airbnb.vercel.app/media/" + src;
 
   return <img {...rest} src={src} />;
 }
